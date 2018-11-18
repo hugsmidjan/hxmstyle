8694aa2acef212abb84a83cb2f573c33e2f182bb
mar <mar.nospam@anomy.net>
2018-11-18 15:07:36 +0000

    feat: Override certain eslint-config-prettier default rules

--- configs/core-prettier.js
+++ configs/core-prettier.js
@@ -1,12 +1,22 @@
+const coreRules = require('./core').rules;
+
+// Override eslint-config-prettier for certain rules
+// this allows us to discourage certain idioms that
+// prettier tolerates but does not require.
+const reInserts = {};
+[ 'curly' ].forEach((key) => { reInserts[key] = coreRules[key]; });
+
+
 module.exports = {
     extends: [
         'plugin:prettier/recommended',
+        // https://github.com/prettier/eslint-config-prettier/blob/master/index.js
         'prettier',
     ],
     plugins: [
         'prettier',
     ],
-    rules: {
-        'prettier/prettier': 'error',
-     },
+    rules: Object.assign({
+        'prettier/prettier': 'warn',
+    }, reInserts),
 };
