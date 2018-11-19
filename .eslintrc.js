2f6a3f9802887d5b0f5355d77def60969694a46d
mar <mar.nospam@anomy.net>
2018-11-19 00:08:00 +0000

    chore: Dogfood hxmstyle

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = require('hxmstyle')({
+    // Place your project-specific additions or overrides here
+    // using standard ESLint config syntax...
+});
---
+++ .prettierrc.js
@@ -0,0 +1,4 @@
+module.exports = require('hxmstyle/prettier')({
+    // Place your project-specific additions or overrides here
+    // using standard Prettierrc syntax...
+});
